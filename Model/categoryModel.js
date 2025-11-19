bd8a06a511fbc5c35fd397f28811c2b83312937a
Monalisa Barik <barikmonalisa171@gmail.com>
2025-11-19 11:38:44 +0530

    update

--- Model/categoryModel.js
+++ Model/categoryModel.js
@@ -1,5 +1,11 @@
 const mongoose= require("mongoose");
 
 const category = new mongoose.Schema({
+ name:{type:String, required:true},
 
-})
+},{
+    timestamps:true,
+}
+);
+
+module.exports = mongoose.model("Category",category);
